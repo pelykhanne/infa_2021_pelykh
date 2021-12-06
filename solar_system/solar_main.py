b87faaa133c5e1cb5a636f23e48b6a5e628fb17d
Anne <91493801+pelykhanne@users.noreply.github.com>
2021-12-06 20:55:25 +0300

    Add files via upload

---
+++ solar_system/solar_main.py
@@ -0,0 +1,210 @@
+# coding: utf-8
+# license: GPLv3
+
+from solar_vis import *
+from solar_model import *
+from solar_input import *
+import thorpy
+import time
+import numpy as np
+import ctypes
+
+timer = None
+
+alive = True
+
+perform_execution = False
+"""Флаг цикличности выполнения расчёта"""
+
+model_time = 0
+"""Физическое время от начала расчёта.
+Тип: float"""
+
+time_scale = 1000.0
+"""Шаг по времени при моделировании.
+Тип: float"""
+
+space_objects = []
+"""Список космических объектов."""
+
+
+def execution(delta):
+    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
+    а также обновляя их положение на экране.
+    Цикличность выполнения зависит от значения глобальной переменной perform_execution.
+    При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
+    """
+    global model_time
+    global displayed_time
+    recalculate_space_objects_positions([dr.obj for dr in space_objects], delta)
+    model_time += delta
+
+
+def start_execution():
+    """Обработчик события нажатия на кнопку Start.
+    Запускает циклическое исполнение функции execution.
+    """
+    global perform_execution
+    perform_execution = True
+
+
+def pause_execution():
+    global perform_execution
+    global space_objects
+    write_space_objects_data_to_file(output, space_objects)
+    perform_execution = False
+
+
+def stop_execution():
+    """Обработчик события нажатия на кнопку Start.
+    Останавливает циклическое исполнение функции execution.
+    """
+    global alive
+    alive = False
+
+
+def open_file_1():
+    """Открывает диалоговое окно выбора имени файла и вызывает
+    функцию считывания параметров системы небесных тел из данного файла.
+    Считанные объекты сохраняются в глобальный список space_objects
+    """
+    global space_objects
+    global browser
+    global model_time
+    global output
+
+    model_time = 0.0
+    in_filename = "solar_system.txt"
+    space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    calculate_scale_factor(max_distance)
+    output="solar_system_out.txt"
+def open_file_2():
+    global space_objects
+    global browser
+    global model_time
+    global output
+
+    model_time = 0.0
+    in_filename = "double_star.txt"
+    space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    calculate_scale_factor(max_distance)
+    output="double_star_out.txt"
+    
+def open_file_3():
+    global space_objects
+    global browser
+    global model_time
+    global output
+
+    model_time = 0.0
+    in_filename = "one_satellite.txt"
+    space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    calculate_scale_factor(max_distance)
+    output="one_satellite_out.txt"
+
+
+def handle_events(events, menu):
+    global alive
+    for event in events:
+        menu.react(event)
+        if event.type == pg.QUIT:
+            alive = False
+
+
+def slider_to_real(val):
+    return np.exp(5 + val)
+
+
+def slider_reaction(event):
+    global time_scale
+    time_scale = slider_to_real(event.el.get_value())
+
+
+def init_ui(screen):
+    global browser
+    slider = thorpy.SliderX(100, (0, 10), "Simulation speed")
+    slider.user_func = slider_reaction
+    button_stop = thorpy.make_button("Quit", func=stop_execution)
+    button_pause = thorpy.make_button("Pause", func=pause_execution)
+    button_play = thorpy.make_button("Play", func=start_execution)
+    Timer = thorpy.OneLineText("Seconds passed")
+
+    button_load_1 = thorpy.make_button(text="Solar system", func=open_file_1)
+    button_load_2 = thorpy.make_button(text="Double star", func=open_file_2)
+    button_load_3 = thorpy.make_button(text="One satellite", func=open_file_3)
+    
+
+    box = thorpy.Box(elements=[
+        slider,
+        button_pause,
+        button_stop,
+        button_play,
+        button_load_1,
+        button_load_2,
+        button_load_3,
+        Timer])
+    reaction1 = thorpy.Reaction(reacts_to=thorpy.constants.THORPY_EVENT,
+                                reac_func=slider_reaction,
+                                event_args={"id": thorpy.constants.EVENT_SLIDE},
+                                params={},
+                                reac_name="slider reaction")
+    box.add_reaction(reaction1)
+
+    menu = thorpy.Menu(box)
+    for element in menu.get_population():
+        element.surface = screen
+
+    box.set_topleft((0, 0))
+    box.blit()
+    box.update()
+    return menu, box, Timer
+
+
+def main():
+    """Главная функция главного модуля.
+    Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
+    """
+
+    global physical_time
+    global displayed_time
+    global time_step
+    global time_speed
+    global space
+    global start_button
+    global perform_execution
+    global timer
+
+    print('Modelling started!')
+    physical_time = 0
+
+    pg.init()
+
+    user32 = ctypes.windll.user32
+    width = user32.GetSystemMetrics(0)
+    height = user32.GetSystemMetrics(1) - 55
+    screen = pg.display.set_mode((width, height))
+    last_time = time.perf_counter()
+    drawer = Drawer(screen)
+    menu, box, timer = init_ui(screen)
+    perform_execution = True
+
+    while alive:
+        handle_events(pg.event.get(), menu)
+        cur_time = time.perf_counter()
+        if perform_execution:
+            execution((cur_time - last_time) * time_scale)
+            text = "%d seconds passed" % (int(model_time))
+            timer.set_text(text)
+
+        last_time = cur_time
+        drawer.update(space_objects, box)
+        time.sleep(1.0 / 60)
+
+    print('Modelling finished!')
+
+
+if __name__ == "__main__":
+    main()
